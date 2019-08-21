24777e22d829b6b34b2c70542d6e241735e074bb
Andrew Chacon <andrew@Andrews-MacBook-Pro.local>
2019-08-20 21:38:18 -0400

    building out dashboard and create profile component

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
+import PrivateRoute from "./components/routing/PrivateRoute";
+import CreateProfile from "./components/profile-forms/CreateProfile";
 
 // Redux
 import { Provider } from "react-redux";
@@ -34,7 +36,12 @@ const App = () => {
             <Switch>
               <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
-              <Route exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
