5996e6ea291129828a21872d068c3d28adc1ce7e
Andrew Chacon <andrew@Andrews-MacBook-Pro.local>
2019-08-31 03:01:14 -0400

    changes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-// Define routes
+// Define  routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
